ac05c28a8b516e465699f75cb6225321a11953ad
Abimael AM <64300722+alvezabimael@users.noreply.github.com>
2021-06-03 00:31:17 -0700

    Practica 1

---
+++ P1/Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] num = {8,5,10,7};
+        int[] num2 = {8,2,4,6};
+        int[] num3 = {7,4,6,4};
+        int[] result = new int[4];
+
+        for(int i =0;i<4;i++){
+            result = findLessThan(num,i);
+            Debug.Log(result[i]);
+        }
+        
+    }
+
+    // El algoritmo empleado es de orden O(n^2), ya que para una entrada N se realizaran N al cuadrado iteraciones.
+    int[] findLessThan(int[] num, int pos)
+    {
+        int[] result = {0,0,0,0};
+        for(int i =0; i<4;i++){
+            if(num[pos] > num[i])
+            result[pos]++;
+        }
+        return result;
+    }
+}
